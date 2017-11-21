5a7d73539d982899c3f7cb217b76742cb2f1542d
Igor Tverdokhleb <itvrd2@yandex.ru>
2017-11-22 01:30:43 +0700

    intervention

--- controllers/main.php
+++ controllers/main.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Capsule\Manager as Capsule;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class Main extends MainController
 {
@@ -25,10 +26,32 @@ class Main extends MainController
             $table->timestamps();
         });
     }
+
     public function migration2()
     {
         Capsule::schema()->table('posts', function (Blueprint $table) {
             $table->string('post_title')->nullable();
         });
+    }
+
+    public function resizeImage()
+    {
+        putenv('GDFONTPATH=' . realpath('.'));
+        $image = Image::make('original.png')
+            ->resize(null, 200, function ($image) {
+                $image->aspectRatio();
+            })
+            ->rotate(45)
+            ->text('watermark', 200, 0, function ($font) {
+                $font->file('arial.ttf');
+                $font->size(48);
+                $font->color('#ccc');
+                $font->align('center');
+                $font->valign('top');
+//                $font->angle(45);
+            })
+            ->save('images/new.png');
+
+        echo "<img src='/images/new.png'>";
     }
 }
